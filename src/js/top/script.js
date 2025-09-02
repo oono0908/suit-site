63a1f3480a508ee60a128c6bb8b8389cc7877d32
oono0908 <damian_wkids@yahoo.co.jp>
2025-09-02 22:38:55 +0900

    mv完了

---
+++ src/js/top/script.js
@@ -0,0 +1,14 @@
+const mySwiper = new Swiper ('.swiper', {
+  loop: true,
+  autoplay: { 
+  delay: 3000,
+  },
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+});
